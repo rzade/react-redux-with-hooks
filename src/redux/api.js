0ec2c7d5ea837343691657705d8521c6bb02802e
rahman <rahmanqadirzade@gmail.com>
2020-03-24 12:24:06 +0300

    'r-r-w-h'

---
+++ src/redux/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default {
+	user: {
+		login: credentials => axios.post(`/api/auth`, { credentials }).then(result => result.data.user),
+		signup: credentials => axios.post(`/api/users`, { credentials }).then(results => results.data.user),
+		currentUser: () => axios.get("/api/users/current_user").then(res => res.data.user)
+	}
+};
